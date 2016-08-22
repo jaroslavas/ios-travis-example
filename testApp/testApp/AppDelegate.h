5992cda71e2ed92d6203183cb4a8283c9e18afbb
Jaroslav <jaroslav@ozevic.com>
2016-08-22 14:54:25 +0300

    Test app

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Jaroslav O on 22/08/16.
+//  Copyright © 2016 j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

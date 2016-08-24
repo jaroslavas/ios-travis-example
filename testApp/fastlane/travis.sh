b606f7485b824f989b2a19926543c8185b7a3400
Jaroslav <jaroslav@ozevic.com>
2016-08-24 15:47:32 +0300

    Updated travis.sh

--- testApp/fastlane/travis.sh
+++ testApp/fastlane/travis.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
 # if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-if [ -z "$DEPLOY_MESSAGE"]; then
-  fastlane test
-  exit $?
+if [ -z "$DEPLOY_MESSAGE" ]; then
+#  fastlane test
+#  exit $?
+# Let's not load travis too much
+    echo "just a test"
+    exit 0;
 fi
 
-if [ -z "$DEPLOY_CHANNEL"]; then
+if [ -z "$DEPLOY_CHANNEL" ]; then
 	echo "No deploy channel set"
 	return 1
 fi
 
+
 # Run agent
+echo "Running agent"
 cd fastlane/agent
 npm install
 cd ../..
-./stlane/agent/agent-cli.sh
+./fastlane/agent/agent-cli.sh
 
 # Run fastlane
+echo "Running fastlane"
 fastlane $DEPLOY_CHANNEL
 exit $?
 

7f54a88b98d19f1af7dd11bda5c849b5586c3957
Jaroslav <jaroslav@ozevic.com>
2016-08-24 15:25:26 +0300

    Little changes in travis.sh to use env variables

--- testApp/fastlane/travis.sh
+++ testApp/fastlane/travis.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 # if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [ -z "$DEPLOY_MESSAGE"]; then
   fastlane test
   exit $?
-# fi
+fi
+
+if [ -z "$DEPLOY_CHANNEL"]; then
+	echo "No deploy channel set"
+	return 1
+fi
+
+fastlane $DEPLOY_CHANNEL
+exit $?
+

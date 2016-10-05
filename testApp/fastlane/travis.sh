97c833743544ae811ad7ae2cb8b18c43408e193b
Jaroslav <jaroslav@ozevic.com>
2016-10-05 09:39:57 +0300

    try

--- testApp/fastlane/travis.sh
+++ testApp/fastlane/travis.sh
@@ -14,7 +14,7 @@ if [ -z "$DEPLOY_CHANNEL" ]; then
 	return 1
 fi
 
-
+export FASTLANE_PASSWORD=$(cat ./fastlane/apple_credentials.sh)
 
 #just to test agents:
 md5 testApp/Assets.xcassets/test_img.imageset/test_img.jpg
